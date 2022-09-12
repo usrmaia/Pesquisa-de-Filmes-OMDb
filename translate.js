692f5f919f237428f17e2806f5e05ce8515af04c
George Maia <georgemaiaf@gmail.com>
2022-09-12 11:03:41 -0300

    refactor

--- translate.js
+++ translate.js
@@ -1,14 +1,13 @@
 async function translate(text){
-  let queryMM = `https://api.mymemory.translated.net/get?q=${text}&langpair=en|pt-br`
-  await fetch(queryMM)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.responseData.translatedText)
-      return data.responseData.translatedText
-    })
-    .catch((reason) => {
-      
-    })
+  let queryMM = `https://api.mymemory.translated.net/get?q=${text.slice(0, 499)}&langpair=en|pt-br`
+  const resp = await fetch(queryMM)
+  const data = await resp.json()
+
+  if (data.responseStatus != 429) {
+    return data.responseData.translatedText
+  } else {
+    return text
+  }
 }
 
 export default translate
